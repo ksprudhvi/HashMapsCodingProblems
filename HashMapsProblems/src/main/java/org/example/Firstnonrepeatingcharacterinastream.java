f743b0116318bb5723b8fd7f4a25c11742ccc2b4
ksprudhvi <ksprudhviofficial@gmail.com>
2023-11-02 21:29:57 +0530

    problem LongestConsicutiveNumbers

--- HashMapsProblems/src/main/java/org/example/Firstnonrepeatingcharacterinastream.java
+++ HashMapsProblems/src/main/java/org/example/Firstnonrepeatingcharacterinastream.java
@@ -7,10 +7,29 @@ public class Firstnonrepeatingcharacterinastream {
     public static void main(String[] args){
         int[] a=new int[] {4,-16,43,4,7,-36,18};
         String  string="wwpyedwrebkbvmvkvvesansqszwtrknvehhgdiqnhietykcgujlclwgyluryjfiaeelhefputiuxtbaedbtlzegpwhmiloosqefh";
-        System.out.println(result(string));
+        System.out.println(result2(string));
 
     }
+/*    Given an input stream A of n characters consisting only of lower case alphabets. While reading characters from the stream, you have to tell which character has appeared only once in the stream upto that point. If there are many characters that have appeared only once, you have to tell which one of them was the first one to appear. If there is no such character then append '#' to the answer.
 
+            NOTE:
+            1. You need to find the answer for every i (0 <= i < n)
+2. In order to find the solution for every i you need to consider the string from starting position till ith position.
+
+
+    Example 1:
+
+    Input: A = "aabc"
+    Output: "a#bb"
+    Explanation: For every ith character we will
+    consider the string from index 0 till index i first non
+    repeating character is as follow-
+            "a" - first non-repeating character is 'a'
+            "aa" - no non-repeating character so '#'
+            "aab" - first non-repeating character is 'b'
+            "aabc" - there are two non repeating characters 'b' and 'c',
+    first non-repeating character is 'b' because 'b' comes before
+'c' in the stream.*/
     private static String result(String string) {
        String result="";
       // result=result+string.charAt(0);
@@ -18,22 +37,18 @@ public class Firstnonrepeatingcharacterinastream {
         //Output: "a#bb"
         HashSet<Character> r= new HashSet();
         Queue<Character> nrq = new  LinkedList<>();
-       // valueQueue.peek()
-
-       // System.out.println(valueQueue.toString());
-     //   valueQueue.remove('b');
-       // System.out.println(valueQueue.toString());
 
         for(int i=0 ;i<string.length();i++){
             if(nrq.contains(string.charAt(i))){
-                nrq.remove(string.charAt(i));
+               // nrq.remove(string.charAt(i));// taking more   time O(n) ----> O(1);
                 if(!r.contains(string.charAt(i))){
                     r.add(string.charAt(i));
                 }
-                if(nrq.isEmpty())
-                    result=result+"#";
-                else
-                    result=result+nrq.peek();
+                    nrq.removeAll(r);
+                    if(nrq.peek()==null ||nrq == null || nrq.isEmpty())
+                        result=result+"#";
+                    else
+                    result = result + nrq.peek();
             }else {
                 if(!r.contains(string.charAt(i)))
                     nrq.add(string.charAt(i));
@@ -51,5 +66,25 @@ public class Firstnonrepeatingcharacterinastream {
         }
 
       return result ;
+    }
+
+    private static String result2(String string) {
+        String result="";
+        // result=result+string.charAt(0);
+        //Input: A = "aabc"
+        //Output: "a#bb"
+        char freq[]= new char[26]; //'z'-'a'=25
+        Queue<Character> nrq = new  LinkedList<>();
+        for(int i=0 ;i<string.length();i++) {
+            nrq.add(string.charAt(i));
+            freq[string.charAt(i)-'a']++;
+            while(!nrq.isEmpty() && freq[nrq.peek()-'a']>1)
+                nrq.remove();
+            if(nrq.peek() ==null)
+                result=result+"#";
+            else
+            result=result+nrq.peek();
+        }
+        return result ;
     }
 }
---
+++ HashMapsProblems/src/main/java/org/example/LongestConsicutiveNumbers.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static jdk.nashorn.internal.objects.NativeArray.sort;
+
+public class LongestConsicutiveNumbers {
+    public static void main(String[] args){
+        int[] a=new int[] {42,37,35,14,17,10,50,20,44,13,4,33,12,28,7,36,46,22,26,21,47,18,31,2,5,34,9,45,16,32,48,19,11,29,27,39,8,49,43};
+        String  string="wwpyedwrebkbvmvkvvesansqszwtrknvehhgdiqnhietykcgujlclwgyluryjfiaeelhefputiuxtbaedbtlzegpwhmiloosqefh";
+        System.out.println(result(a,39));
+
+    }
+    /*Given an array of positive integers. Find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
+
+
+            Example 1:
+
+    Input:
+    N = 7
+    a[] = {2,6,1,9,4,5,3}
+    Output:
+            6*/
+    private static int result(int arr[], int N) {
+
+        Arrays.sort(arr);
+        int secondmax;
+        int max;
+        int count =1;
+        int maxCount=1;
+        for(int i=0;i<N-1 ;i++){
+            secondmax=arr[i];
+            max=arr[i+1];
+            if(max-secondmax>1){
+                if(count>maxCount)
+                maxCount=count;
+                 count=1;
+            }else if(max-secondmax==0)
+                continue;
+            else
+             count++;
+        }
+
+        return maxCount>count?maxCount:count;
+    }
+
+}
